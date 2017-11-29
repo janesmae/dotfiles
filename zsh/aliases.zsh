c4402dee17590fd9d398384a7916ee710fdb9932
Jaan Janesmae <git@janesmae.com>
2017-11-29 09:55:31 +0200

    zsh: add update! alias and completion

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,12 +19,15 @@ alias dns_flush="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;sa
 # Mute: http://xkcd.com/530/
 alias mute="osascript -e 'set volume output muted true'"
 
-# Reload zsh config
-alias reload!='RELOAD=1 source ~/.zshrc'
-
 # Helpers
-alias df='df -h' # disk free, in Gigabytes, not bytes
-alias du='du -h -c' # calculate disk usage for a folder
+alias df='df -h'		# disk free, in Gigabytes, not bytes
+alias du='du -h -c'		# disk usage, for a folder
 
 # Start using Vim8
 alias vi=vim
+
+# Reload zsh config
+alias reload!='RELOAD=1 source ~/.zshrc'
+
+# Update system
+alias update!="brew update;brew upgrade;brew cleanup;brew cask outdated | xargs brew cask reinstall;mas upgrade"
