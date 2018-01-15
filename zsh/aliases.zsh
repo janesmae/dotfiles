7399f2742c48b146960d2fa4f87df790b4f5e48b
Jaan Janesmae <git@janesmae.com>
2018-01-15 10:03:29 +0200

    zsh: separate mac and linux dependent aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,12 +13,6 @@ alias grep="grep --color=auto"
 alias fgrep="fgrep --color=auto"
 alias egrep="egrep --color=auto"
 
-# Macos dns flush
-alias dns_flush="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;say cache flushed"
-
-# Mute: http://xkcd.com/530/
-alias mute="osascript -e 'set volume output muted true'"
-
 # Helpers
 alias df='df -h'		# disk free, in Gigabytes, not bytes
 alias du='du -h -c'		# disk usage, for a folder
@@ -29,5 +23,24 @@ alias vi=vim
 # Reload zsh config
 alias reload!='RELOAD=1 source ~/.zshrc'
 
-# Update system
-alias update!="brew update;brew upgrade;brew cleanup;brew doctor;brew cask upgrade;mas upgrade"
+# OS specific configurations
+case `uname` in
+	Darwin)
+		# OS X
+
+		# Macos dns flush
+		alias dns_flush="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;say cache flushed"
+
+		# Mute: http://xkcd.com/530/
+		alias mute="osascript -e 'set volume output muted true'"
+
+		# Update system
+		alias update!="brew update;brew upgrade;brew cleanup;brew doctor;brew cask upgrade;mas upgrade"
+
+	;;
+	Linux)
+		# Linux
+
+	;;
+
+esac
