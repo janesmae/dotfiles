57b89634b55ff8b2e986532fa4d034d72d031a03
Jaan Janesmae <git@janesmae.com>
2022-10-18 13:12:43 +0300

    update update command

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -46,7 +46,7 @@ case `uname` in
 			brew upgrade --cask
 			# mas upgrade
 			# gem update
-			pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
+			pip3 list --outdated | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
 		}
 
 	;;
